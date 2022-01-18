9aa1609df35735fe64ab5a4123667c894279349b
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-01-18 20:04:29 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://st4.depositphotos.com/18664664/23958/v/450/depositphotos_239583254-stock-illustration-droid-icon-trendy-droid-logo.jpg'></img>
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import '../App.css';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src='http://htmlbook.ru/files/images/layout2/6-05.png'></img>
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					post 1
+				</div>
+				<div>
+					post 2
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
