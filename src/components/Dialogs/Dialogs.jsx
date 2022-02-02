4875a693217a296c027920557685bccd641ed7f0
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-02-03 00:08:53 +0300

    21 Dialogues started

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,20 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>Dima</div>
+                <div className={classes.dialog}>Alexey</div>
+                <div className={classes.dialog}>Katya</div>
+                <div className={classes.dialog}>Nastya</div>
+                <div className={classes.dialog}>Sasha</div>
+                <div className={classes.dialog}>Kirill</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>How are you?</div>
+            </div>
         </div>
     );
 }
