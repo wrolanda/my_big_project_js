e45fb7e0281df24f12c215c9a53fb7f6ba84a373
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-02-16 15:10:18 +0300

    24 UI - BLL, push data aside

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,20 +18,36 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Alexey'},
+        {id: 3, name: 'Katya'},
+        {id: 4, name: 'Nastya'},
+        {id: 5, name: 'Sasha'},
+        {id: 6, name: 'Kirill'}
+    ]
+
+    let messageData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Hello'},
+        {id: 3, message: 'How are you?'}
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name="Dima" id="1"/>
-                <DialogItem name="Alexey" id="2"/>
-                <DialogItem name="Katya" id="3"/>
-                <DialogItem name="Nastya" id="4"/>
-                <DialogItem name="Sasha" id="5"/>
-                <DialogItem name="Kirill" id="6"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
             </div>
             <div className={classes.messages}>
-                <Message message="Hi!"/>
-                <Message message="Hello"/>
-                <Message message="How are you?"/>
+                <Message message={messageData[0].message}/>
+                <Message message={messageData[1].message}/>
+                <Message message={messageData[2].message}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,11 +2,18 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: 'Hello, World!', likesCount: '42'},
+        {id: 2, message: 'It\'s my first post', likesCount: '21'},
+        //{id: 3, mem: {<img src='https://cs14.pikabu.ru/post_img/big/2021/06/28/10/1624898051168416526.jpg'></img>}, likesCount: '10'}
+]
+
     return (
         <div>
-			<div>
-				<h3>My posts</h3>
-			</div>
+            <div>
+                <h3>My posts</h3>
+            </div>
             <div>
                 <textarea></textarea>
             </div>
@@ -15,8 +22,8 @@ const MyPosts = () => {
                 <button>Remove</button>
             </div>
             <div className={classes.message}>
-                <Post message="Hello, World!" likesCount='42'/>
-                <Post message="It's my first post" likesCount='21'/>
+                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
                 <Post mem={<img src='https://cs14.pikabu.ru/post_img/big/2021/06/28/10/1624898051168416526.jpg'></img>}
                       likesCount='10'/>
             </div>
