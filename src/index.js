f9775f56e424b9b23d110238a219e26805b2f875
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-03-19 15:56:02 +0300

    subscribe, observer, rerenderEntire callback f

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
-import {rerenderEntireTree} from './render'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import state, {subscribe} from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
+import {addMessage, addPost, updateNewMessageText, updateNewPostText} from "./redux/state";
+import reportWebVitals from "./reportWebVitals";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state}
+                     addPost={addPost}
+                     addMessage={addMessage}
+                     updateNewPostText={updateNewPostText}
+                     updateNewMessageText={updateNewMessageText}/>
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log('State changed');
+}
 
 let state = {
     dialogsPage: {
@@ -27,7 +29,9 @@ let state = {
     }
 }
 
-export     let addPost = () => {
+window.state = state;
+
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -38,7 +42,7 @@ export     let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export     let addMessage = () => {
+export const addMessage = () => {
     let newMessage = {
         id: 5,
         message: state.dialogsPage.newMessageText
@@ -48,14 +52,18 @@ export     let addMessage = () => {
     rerenderEntireTree(state);
 }
 
-export     let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
-export     let updateNewMessageText = (newText) => {
+export const updateNewMessageText = (newText) => {
     state.dialogsPage.newMessageText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
