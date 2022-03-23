54ff7cec4d033558aa1bee68d39f431ee2ad4c5f
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-03-23 14:47:11 +0300

    37 refactoring state and other methods in store object

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,70 +1,67 @@
-let rerenderEntireTree = () => {
-    console.log('State changed');
-}
-
-let state = {
-    dialogsPage: {
-        dialogsData: [
-            {id: 1, name: 'Dima'},
-            {id: 2, name: 'Alexey'},
-            {id: 3, name: 'Katya'},
-            {id: 4, name: 'Nastya'},
-            {id: 5, name: 'Sasha'},
-            {id: 6, name: 'Kirill'}
-        ],
-        messagesData: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'Hello'},
-            {id: 3, message: 'How are you?'}
-        ],
-        newMessageText: ''
+let store = {
+    _state: {
+        dialogsPage: {
+            dialogsData: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Alexey'},
+                {id: 3, name: 'Katya'},
+                {id: 4, name: 'Nastya'},
+                {id: 5, name: 'Sasha'},
+                {id: 6, name: 'Kirill'}
+            ],
+            messagesData: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Hello'},
+                {id: 3, message: 'How are you?'}
+            ],
+            newMessageText: ''
+        },
+        profilePage: {
+            postsData: [
+                {id: 1, message: 'Hello, World!', likesCount: '42'},
+                {id: 2, message: 'It\'s my first post', likesCount: '21'},
+                //{id: 3, mem: {<img src='https://cs14.pikabu.ru/post_img/big/2021/06/28/10/1624898051168416526.jpg'></img>}, likesCount: '10'}
+            ],
+            newPostText: ''
+        }
     },
-    profilePage: {
-        postsData: [
-            {id: 1, message: 'Hello, World!', likesCount: '42'},
-            {id: 2, message: 'It\'s my first post', likesCount: '21'},
-            //{id: 3, mem: {<img src='https://cs14.pikabu.ru/post_img/big/2021/06/28/10/1624898051168416526.jpg'></img>}, likesCount: '10'}
-        ],
-        newPostText: ''
-    }
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('State changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    addMessage() {
+        let newMessage = {
+            id: 5,
+            message: this._state.dialogsPage.newMessageText
+        };
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 }
 
-window.state = state;
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.postsData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 5,
-        message: state.dialogsPage.newMessageText
-    };
-    state.dialogsPage.messagesData.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
-
+export default store;
+window.store = store;
