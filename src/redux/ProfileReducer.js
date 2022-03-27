227fd34052b2f04c6c5e010484be4c11d07806db
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-03-27 23:08:06 +0300

    connection redux and adaptation state action and reducers

--- src/redux/ProfileReducer.js
+++ src/redux/ProfileReducer.js
@@ -1,7 +1,16 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const ProfileReducer = (state, action) => {
+let initialState = {
+        postsData: [
+            {id: 1, message: 'Hello, World!', likesCount: '42'},
+            {id: 2, message: 'It\'s my first post', likesCount: '21'},
+            //{id: 3, mem: {<img src='https://cs14.pikabu.ru/post_img/big/2021/06/28/10/1624898051168416526.jpg'></img>}, likesCount: '10'}
+        ],
+        newPostText: ''
+};
+
+const ProfileReducer = (state = initialState, action) => {
     switch (action.type) {
 
         case (ADD_POST):
