9a1b52a1b7e36b44e407a1ae352868cfbef87a81
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-03-29 22:03:12 +0300

    ReduxStore from 42 and 47- CopyState in ProfileReducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from "./redux/state";
+import store from "./redux/ReduxStore";
 import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
@@ -22,7 +22,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/ProfileReducer.js
+++ src/redux/ProfileReducer.js
@@ -1,23 +1,37 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const ProfileReducer = (state, action) => {
-    switch (action.type) {
+let initialState = {
+    postsData: [
+        {id: 1, message: 'Hello, World!', likesCount: '42'},
+        {id: 2, message: 'It\'s my first post', likesCount: '21'},
+        //{id: 3, mem: {<img src='https://cs14.pikabu.ru/post_img/big/2021/06/28/10/1624898051168416526.jpg'></img>}, likesCount: '10'}
+    ],
+    newPostText: ''
+}
 
-        case (ADD_POST):
+const ProfileReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case (ADD_POST): {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             };
+            let stateCopy = {...state};
             if (newPost.message !== "") {
-                state.postsData.push(newPost);
+                stateCopy.postData = [...state.postsData];
+                stateCopy.postsData.push(newPost);
             }
-            state.newPostText = '';
-            return state;
-        case (UPDATE_NEW_POST_TEXT):
-            state.newPostText = action.newText;
-            return state;
+                stateCopy.newPostText = '';
+                return stateCopy;
+        }
+        case (UPDATE_NEW_POST_TEXT): {
+            let stateCopy = {...state};
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
