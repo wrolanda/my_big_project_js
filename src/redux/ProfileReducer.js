31275d637c94d7af5d50523cf9cbde58351f38da
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-03-29 22:03:12 +0300

    ReduxStore from 42 and 47- CopyState in ProfileReducer

--- src/redux/ProfileReducer.js
+++ src/redux/ProfileReducer.js
@@ -1,23 +1,35 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const ProfileReducer = (state, action) => {
-    switch (action.type) {
+let initialState = {
+    postsData: [
+        {id: 1, message: 'Hello, World!', likesCount: '42'},
+        {id: 2, message: 'It\'s my first post', likesCount: '21'},
+        //{id: 3, mem: {<img src='https://cs14.pikabu.ru/post_img/big/2021/06/28/10/1624898051168416526.jpg'></img>}, likesCount: '10'}
+    ],
+    newPostText: ''
+}
 
-        case (ADD_POST):
+const ProfileReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case (ADD_POST): {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             };
-            if (newPost.message !== "") {
-                state.postsData.push(newPost);
-            }
-            state.newPostText = '';
-            return state;
+            let stateCopy = {...state}
+            // if (newPost.message !== "") {
+            stateCopy.postData = [...state.postsData];
+            stateCopy.postsData.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;
+        }
         case (UPDATE_NEW_POST_TEXT):
-            state.newPostText = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
         default:
             return state;
     }
