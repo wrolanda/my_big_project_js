bcf3bd8fd06fcb872f0fe6d59387542958fbbc9b
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-04-03 15:27:22 +0300

    50#6 ajax в деталях: todo-list api, get, post

--- requests/index.js
+++ requests/index.js
@@ -1,17 +1,39 @@
 const resultBlock = document.querySelector("#result");
 const pageNumberEl = document.querySelector("#page-number");
-const clickMeButton = document.querySelector("#click-me");
+const getImagesButton = document.querySelector("#get-images");
+const getTasksButton = document.querySelector("#get-tasks");
 
-clickMeButton.addEventListener("click", () => {
+
+getImagesButton.addEventListener("click", () => {
 	const promise = getImages(pageNumberEl.value);
 	promise
-		.then(onDataReceived);
+		.then(onImagesReceived);
 });
 
-function onDataReceived(array) {
+getTasksButton.addEventListener("click", () => {
+	const promise = getTasks();
+	promise
+		.then(onTasksReceived);
+});
+
+createTask('text1').then((data) => {
+	debugger;
+	console.log(data);
+})
+
+function onImagesReceived(array) {
 	array.forEach(el => {
 		const img = document.createElement('img');
 		img.src = el.thumbnail;
-		document.querySelector('#result').appendChild(img);
+		document.querySelector('#images-result').appendChild(img);
 	});
 }
+
+function onTasksReceived(tasks) {
+	tasks.forEach(task => {
+		const li = document.createElement('li');
+		li.innerHTML = task.title;
+		document.querySelector('#tasks-result').appendChild(li);
+	});
+}
+
--- requests/service.js
+++ requests/service.js
@@ -1,6 +1,19 @@
 function getImages(pageNumber) {
-
 	const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+	return promise.then((response) => {
+		return response.data;
+	});
+};
+
+function getTasks() {
+	const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=112`);
+	return promise.then((response) => {
+		return response.data;
+	});
+};
+
+function createTask(title) {
+	const promise = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=112&title=${title}`);
 	return promise.then((response) => {
 		return response.data;
 	});
