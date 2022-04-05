75e1e7cbedd5718ef2d80beb01f71afffe070830
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-04-05 17:46:56 +0300

    52 Side Effect, added button: GetUsers, for pure function

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -13,15 +13,18 @@ import userImg from '../../assets/images/user-default.png'
 // },
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items);
-            })
-    }
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+                })
+            }
+        };
 
     return (
         <div>
+            <button onClick={ getUsers }>Get users</button>
             {
                 props.users.map(u =>
                     <div key={u.id}>
