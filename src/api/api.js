518015e830aed6c45be684058a645b48dfc4f97b
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-04-21 14:58:48 +0300

    63 DAL + axios.create

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import * as axios from "axios";
 
-const instance = axios.create({
+const instanceAuth = axios.create({
     withCredentials: true,
     baseURL: "https://social-network.samuraijs.com/api/1.0/",
     headers: {
@@ -8,51 +8,36 @@ const instance = axios.create({
     },
 });
 
+const instanceNoAuth = axios.create({
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+});
+
+export const usersAPI = {};
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-        {
-            withCredentials: true,
-        }).then (response => {
-            return response.data;
+    return instanceAuth.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+        return response.data;
     })
 };
 
 export const getProfile = (userId) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
+    return instanceNoAuth.get(`profile/${userId}`)
 };
 
 export const authMe = () => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`,{
-        withCredentials: true
-    }).then (response => {
+    return instanceAuth.get(`auth/me`).then(response => {
         return response.data;
     })
 };
 
 export const unfollowAPI = (id) => {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`,
-        {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "cffd1188-ee84-4dfc-8a84-b95e15dd0748"
-            }
-        })
-        .then(response => {
-          return response.data;
-        })
+    return instanceAuth.delete(`follow/${id}`).then(response => {
+        return response.data;
+    })
 };
 
 export const followAPI = (id) => {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`,
-        {},
-        {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "cffd1188-ee84-4dfc-8a84-b95e15dd0748"
-            }
-        })
-        .then(response => {
-            return response.data;
-        })
+    return instanceAuth.post(`follow/${id}`, {}).then(response => {
+        return response.data;
+    })
 };
